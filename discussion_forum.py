04cd75d11d179dc062dca454dff758752d1aa59e
B3KALB <B3KALB@gmail.com>
2022-09-29 10:57:10 -0500

    DF-printValue
    print function to show the return

--- discussion_forum.py
+++ discussion_forum.py
@@ -1,4 +1,7 @@
+yup = "cOULD YOU aPPLY A pRINT STATEMENT?"
+
 # 1
+
 
 def any_lowercase1(s):
      for c in s:
@@ -18,6 +21,8 @@ def any_lowercase2(s):
           else:
                return 'False'
 
+lowercase = any_lowercase2(yup)
+print(lowercase)
 
 # 3
 
@@ -26,6 +31,8 @@ def any_lowercase3(s):
           flag = c.islower()
      return flag
 
+lowercase = any_lowercase3(yup)
+print(lowercase)
 
 # 4
 
@@ -35,6 +42,9 @@ def any_lowercase4(s):
           flag = flag or c.islower()
      return flag
 
+lowercase = any_lowercase4(yup)
+print(lowercase)
+
 # 5
 
 def any_lowercase5(s):
@@ -43,3 +53,5 @@ def any_lowercase5(s):
                return False
      return True
 
+lowercase = any_lowercase5(yup)
+print(lowercase)
