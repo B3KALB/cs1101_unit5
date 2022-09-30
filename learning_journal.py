744a868182c82c9cb07f9584067ea2a05fba3e01
B3KALB <B3KALB@gmail.com>
2022-09-30 16:03:26 -0500

    LearningJournal_unit5
    Learning Journal Unit 5
    Welcome to the Unit 5 Learning Journal!
    
    1. Consider the loop from Section 8.3 of your textbook.
    
    prefixes = 'JKLMNOPQ'
    suffix = 'ack'
    
    for letter in prefixes:
         print(letter + suffix)
    
    Put this code into a Python script and run it. Notice that it prints the names "Oack" and "Qack".
    
    Modify the program so that it prints "Ouack" and "Quack" but leaves the other names the same.
    
    Include the modified Python code and the output in your submission.
    
    2. Give at least three examples that show different features of string slices. Describe the feature illustrated by each example. Invent your own examples. Do not copy them for the textbook or any other source.
    
    The code and its output must be explained technically. The explanation can be provided before or after the code, or in the form of comments within the code. For code modification type of questions, always mention or clearly highlight the part which is modified, along with the reason stated, as a code comment. The descriptive part of your response must be at least 200 words.
    
    If you use an informational source, be sure to identify the source and share the link to the source you used. This is a good time to start practicing some of what you learned about APA in UNIV 1001.

--- learning_journal.py
+++ learning_journal.py
@@ -3,25 +3,29 @@ suffix = 'ack'
 buy_a_vowel = 'u'
 
 for letter in prefixes:
+
     if letter == 'O':
+       
         print(letter + buy_a_vowel + suffix)
+    
     elif letter == 'Q':
+       
         print(letter + buy_a_vowel + suffix)
+    
     else:
+       
         print(letter + suffix)
 
 #  Give at least three examples that show different features of string slices. 
 # Describe the feature illustrated by each example. 
 # Invent your own examples. Do not copy them for the textbook or any other source.
 
-words_one = "Stamps are better than bottle caps."
-words_two = "Simple is,"
-words_three = "sempla"
-vowel_string = 'aeiou'
-
 # ex2.1
+
+words_one = "Stamps are better than bottle caps."
+
 def use_words_one(one):
-    
+  
     print(one)
     
     better_than = one
@@ -30,11 +34,13 @@ def use_words_one(one):
     
     print(new_words)
     
-
 use_words_one(words_one)
 
 
 # ex2.2
+
+words_two = "Simple is,"
+
 def use_words_two(two):
     
     print(two)
@@ -45,14 +51,72 @@ def use_words_two(two):
 
     print(new_word)
     
-    
 use_words_two(words_two)
 
 
 # ex2.3
 
+words_three = "sempla"
+vowel_string = 'aeiou'
+
 def use_words_three(three, vowels):
 
-    better_than = three, vowels 
+    print(f'{three} is now:')
+    
+    fix_the_i = three[0:1] + vowels[2:-2] + three[2:]
+    
+    fix_the_e = fix_the_i[0:-1] + vowels[1:-3]
 
-use_words_one(words_three, vowel_string)
+    print(fix_the_e)
+    
+use_words_three(words_three, vowel_string)
+
+
+# playing around
+scramble = input('Scramble: ')
+def use_scrable(clean_text):
+    vowel_1 = 'a'
+    vowel_2 = 'e'
+    vowel_3 = 'i'
+    vowel_4 = 'o'
+    vowel_5 = 'u'
+    new_word = ''
+    for letters in clean_text:
+        if letters == 'a':
+            new_word += vowel_2
+        elif letters == 'e':
+            new_word += vowel_3
+        elif letters == 'i':
+            new_word += vowel_4
+        elif letters == 'o':
+            new_word += vowel_5
+        elif letters == 'u':
+            new_word += vowel_1
+        else:
+            new_word += letters
+    print(new_word)
+use_scrable(scramble)
+
+decode = input('Descramble: ')
+def use_decode(code_word):
+    vowel_1 = 'a'
+    vowel_2 = 'e'
+    vowel_3 = 'i'
+    vowel_4 = 'o'
+    vowel_5 = 'u'
+    new_word = ''
+    for letters in code_word:
+        if letters == 'i':
+            new_word += vowel_2
+        elif letters == 'o':
+            new_word += vowel_3
+        elif letters == 'u':
+            new_word += vowel_4
+        elif letters == 'a':
+            new_word += vowel_5
+        elif letters == 'e':
+            new_word += vowel_1
+        else:
+            new_word += letters
+    print(new_word)
+use_decode(decode)
