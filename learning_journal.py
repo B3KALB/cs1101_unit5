fd4e46f6db5eaeaf74e66c25e25bfb0ac952d8bf
B3KALB <B3KALB@gmail.com>
2022-09-30 11:06:39 -0500

    unit5_lj_ex2.1
    2. Give at least three examples that show different features of string slices. Describe the feature illustrated by each example. Invent your own examples. Do not copy them for the textbook or any other source.

--- learning_journal.py
+++ learning_journal.py
@@ -15,26 +15,44 @@ for letter in prefixes:
 # Invent your own examples. Do not copy them for the textbook or any other source.
 
 words_one = "Stamps are better than bottle caps."
-words_two = "simple"
+words_two = "Simple is,"
 words_three = "sempla"
 vowel_string = 'aeiou'
 
 # ex2.1
 def use_words_one(one):
+    
     print(one)
+    
     better_than = one
-    new_greeting = 'Camping is' + better_than[10:23] + 'staying in a hotel.'
-    print(new_greeting)
+    
+    new_words = 'Camping is' + better_than[10:23] + 'staying in a hotel.'
+    
+    print(new_words)
+    
+
 use_words_one(words_one)
+
 
 # ex2.2
 def use_words_two(two):
+    
+    print(two)
+
     better_than = two 
 
+    new_word = better_than[:-5] + 'y changed.'
+
+    print(new_word)
+    
+    
 use_words_two(words_two)
 
+
 # ex2.3
+
 def use_words_three(three, vowels):
+
     better_than = three, vowels 
 
 use_words_one(words_three, vowel_string)
